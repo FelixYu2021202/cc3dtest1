43091760cb32d7fa61dde0c92a5643e596689b80
faace <solo1234>
2020-02-02 14:26:38 -0500

    11

--- assets/scence/scMain.ts
+++ assets/scence/scMain.ts
@@ -1,32 +1,20 @@
-import { _decorator, Component, Node, AnimationComponent, SystemEvent, EventMouse, systemEvent } from "cc";
-import { gv } from "../scripts/glubals/gv";
-const { ccclass, property } = _decorator;
+import * as cc from "cc"
+const { ccclass, property } = cc._decorator;
 
-@ccclass("Main")
-export class Main extends Component {
+@ccclass("scMain")
+export class scMain extends cc.Component {
     /* class member could be defined like this */
     // dummy = '';
 
     /* use `property` decorator if your want the member to be serializable */
-    @property({ type: AnimationComponent })
-    cubeAnim: AnimationComponent = null;
-    @property({ type: Node })
-    cube: Node = null;
+    @property({ type: cc.Prefab })
+    public pfChurch: cc.Prefab = null;
     // serializableDummy = 0;
 
     start() {
-        // gv.gf.keyDown(this.cube, (event: EventMouse) => {
-        //     this.cubeAnim.play("scMain");
-        // });
-        systemEvent.on(SystemEvent.EventType.TOUCH_START, (event: EventMouse) => {
-            console.log(1);
-            if (event.getButton() === 0 || event.getButton() === 2) console.log(2);
-        });
-        systemEvent.on(SystemEvent.EventType.MOUSE_DOWN, (event: EventMouse) => {
-            console.log(3);
-            if (event.getButton() === 0 || event.getButton() === 2) console.log(4);
-        });
-
+        // Your initialization goes here.
+        let church = cc.instantiate(this.pfChurch);
+        church.scale.x = 302
     }
 
     // update (deltaTime: number) {
