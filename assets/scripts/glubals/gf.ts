642c0246ed8e396acb8d199c70ddffeb43df263b
felix <1276273366@qq.com>
2020-02-02 15:00:00 -0500

    version updated:0.0.1

--- assets/scripts/glubals/gf.ts
+++
@@ -1,9 +0,0 @@
-import { Node } from "cc";
-export let gf = {
-    addClick: (th: Node, func: any) => {
-        th.on("touch-start", func);
-    },
-    keyDown: (th: Node, func: any) => {
-        th.on("keydown", func);
-    },
-}
