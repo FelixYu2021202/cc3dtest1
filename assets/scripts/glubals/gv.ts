3986586bafd824cb574afa067d79eba4d4ea2cee
felix <1276273366@qq.com>
2020-02-07 14:59:55 -0500

    aaa

---
+++ assets/scripts/glubals/gv.ts
@@ -0,0 +1,7 @@
+import { gf } from "./gf";
+import { ggv } from "./ggv";
+
+export let gv = {
+    gf: gf,
+    gg: ggv,
+}
---
+++ test1/assets/scripts/glubals/ggv.ts
@@ -0,0 +1,6 @@
+export let ggv = {
+    creater: "Felix",
+    version: "v0.0.1",
+    name: "1st test game",
+    adresse: '/script/glubals/',
+}
