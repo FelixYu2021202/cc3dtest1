c625a53f17095b5962ddaab4529311d1b031f9ae
faace <solo1234>
2020-02-08 13:15:47 -0500

    versioned 0.0.2

---
+++ test2/assets/scenes/sc3.ts
@@ -0,0 +1,31 @@
+// import { _decorator, Component, Node, ParticleSystemComponent } from "cc";
+// const { ccclass, property } = _decorator;
+
+// @ccclass("sc3")
+// export class sc3 extends Component {
+//     /* class member could be defined like this */
+//     // dummy = '';
+
+//     ;
+
+//     /* use `property` decorator if your want the member to be serializable */
+//     @property({ type: ParticleSystemComponent })
+//     private ps1: ParticleSystemComponent = null;
+//     // serializableDummy = 0;
+
+//     private Size = this.ps1.SizeOvertimeModule.Size;
+
+//     start() {
+//         this.ps1.SizeOvertimeModule.SeparateAxes = false;
+//         // Your initialization goes here.
+//     }
+
+//     update(deltaTime: number) {
+//         if (this.Size == 15) {
+//             this.Size = 5;
+//         } else {
+//             this.Size++;
+//         }
+//         // Your update function goes here.
+//     }
+// }
